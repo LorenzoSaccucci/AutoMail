395d907938d313c46d39beff430428af9cf8b871
SamueleProietti <116893718+SamueleProietti@users.noreply.github.com>
2023-04-04 17:11:54 +0200

    Update 02_Gruppi.py

--- pages/02_Gruppi.py
+++ pages/02_Gruppi.py
@@ -7,7 +7,7 @@ import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode
 
 
-st.set_page_config(page_title='Inserimento', layout = 'wide', initial_sidebar_state = 'auto')
+st.set_page_config(page_title='Gruppi', layout = 'wide', initial_sidebar_state = 'auto')
 hide_streamlit_style = """
             <style>
             footer {visibility: hidden;}
@@ -15,6 +15,7 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
+st.title("Gruppi")
 # --- collegamento con la chiave di autenticazione per il database in firebase ---
 if not firebase_admin._apps:
         cred = credentials.Certificate('firestore-key.json')
@@ -33,10 +34,53 @@ listass = col1.multiselect("Scegli gli associati che vuoi inserire nel gruppo", 
 group_name = col2.text_input("Inserisci il nome del gruppo:")
 
 if st.button("Aggiungi gruppo"):
+        
+        if group_name == '':
+                st.warning('⚠️ Inserisci un nome valido al grouppo')
+        if len(listass)<2:
+                st.warning('⚠️ Inserisci almeno due nomi dalla lista per creare un gruppo')
 
-        db.collection(u'Groups').add({
+        db.collection(u'Gruppi').document("grupppo_"+group_name).set({
                 u'maillist': listass,
                 u'name': group_name
         })
 
         st.success("Il Gruppo è stato aggiunto con successo!")
+# --- Modalità di lettura di una specifica raccolta all'interno del database
+doc_ref = db.collection(u"Gruppi")
+# --- Modalità di lettura dei documenti all'interno della raccolta
+docs = doc_ref.stream()
+
+# --- Costruzione di una tabella dei dati del database
+people = []
+for doc in docs:
+        #st.write(doc)
+        people_dict = {'mail list': doc.to_dict()['maillist'], 'name': doc.to_dict()['name']}
+        people.append(people_dict)
+
+if people!=[]:
+        data = pd.DataFrame(people)
+        gd = GridOptionsBuilder.from_dataframe(data)
+        gd.configure_selection(selection_mode='multiple', use_checkbox=True)
+        gd.configure_grid_options(enableCellTextSelection=True)
+        gd.configure_grid_options(ensureDomOrder=True)
+        gd.configure_pagination(enabled=True, paginationAutoPageSize=False, paginationPageSize=6)
+
+        gridOptions = gd.build()
+
+        table = AgGrid(data, gridOptions=gridOptions, update_mode=GridUpdateMode.SELECTION_CHANGED, enable_enterprise_modules=False, height=270, fit_columns_on_grid_load=True)
+
+        selected = table['selected_rows']
+
+
+
+        if st.button('Elimina gruppi selezionati'):
+                for dict in selected:
+                        db.collection(u'Gruppi').document(dict['gruppo_'+dict['name']]).delete()
+
+                        st.success('Eliminazione effettuata')
+                        time.sleep(1)
+                        st.experimental_rerun()
+
+else:
+        st.warning('Nessun gruppo registrato')
